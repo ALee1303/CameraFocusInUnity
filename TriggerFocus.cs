3fcec14dd9cf3b3548447027ba6311df9b9b2db1
Alex Lee <asylee1005@gmail.com>
2018-04-16 01:39:46 -0500

    fix comment on TriggerFocus

--- TriggerFocus.cs
+++ TriggerFocus.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 /// Player must be tagged with default tag "Player"
 /// player's first child must be MainCamera
 /// Script must be attached to the trigger.
-/// trigger's first child must be empty object with transform facing the zoom view.
+/// trigger's first child must be empty object with transform facing the focus view.
 /// </summary>
 public class TriggerFocus : FocusObject
 {
